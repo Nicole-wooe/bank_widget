43f622ba3549afc487f9c53258e0bfa3920bcd64
Nicol <nicol@example.com>
2026-05-14 20:15:47 +0300

    Добавлены магические методы для товаров и категорий

---
+++ src/bank_widget/category.py
@@ -0,0 +1,35 @@
+from src.bank_widget.product import Product
+
+
+class Category:
+    category_count = 0
+    product_count = 0
+
+    def __init__(self, name: str, description: str, products: list[Product]):
+        self.name = name
+        self.description = description
+        self.__products = products
+
+        Category.category_count += 1
+        Category.product_count += len(products)
+
+    def add_product(self, product):
+        self.__products.append(product)
+        Category.product_count += 1
+
+    @property
+    def products(self):
+        result = ""
+
+        for product in self.__products:
+            result += f"{str(product)}\n"
+
+        return result
+
+    def __str__(self) -> str:
+        total_quantity = 0
+
+        for product in self.__products:
+            total_quantity += product.quantity
+
+        return f"{self.name}, количество продуктов: {total_quantity} шт."
